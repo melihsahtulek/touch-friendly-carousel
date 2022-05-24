60761b2138ce230333b09e1a035064f3ff9fec4d
mtulek <melihsahtulek@gmail.com>
2022-05-24 15:10:54 +0300

    init app

---
+++ carousel.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", () => {
+  const carousels = document.querySelectorAll('[data-name="carousel"]');
+  const controlButtons = document.querySelectorAll(".carousel-control");
+
+  // EVENTS
+  const nextItem = () => {};
+  const prevItem = () => {};
+  const touchStart = () => {};
+  const touchMove = () => {};
+  const touchStop = () => {};
+  const touchCancel = () => {};
+
+  for (const carousel of carousels) {
+  }
+});
