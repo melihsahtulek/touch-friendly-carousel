6bcd4e1db3316bf05ef5a00fb949fbd98b2299d9
mtulek <melihsahtulek@gmail.com>
2022-05-30 14:35:32 +0300

    wip

--- carousel.js
+++ carousel.js
@@ -3,15 +3,32 @@ window.addEventListener("load", () => {
   const controlButtons = document.querySelectorAll(".carousel-control");
 
   let firstTouchX = 0,
-    firstTouchY = 0,
     control = false,
     index = 0,
-    width = carousel.clientWidth + 2;
+    width = carousel.clientWidth + 2,
+    direction = "next",
+    lastMoveX = 0,
+    maxCount = 0;
 
-  const move = (n) => {
-    for (const elem of carousel.children) {
-      if (elem.tagName.toLocaleLowerCase() === "div") {
-        elem.style.transform = `translateX(${n}px)`;
+  const move = (n, obj) => {
+    lastMoveX = n;
+    maxCount = 0;
+
+    if (obj.delay) {
+      for (const elem of carousel.children) {
+        if (elem.tagName.toLocaleLowerCase() === "div") {
+          maxCount++;
+          elem.style.transform = `translateX(${n}px)`;
+          elem.style.transition = ".25s";
+        }
+      }
+    } else {
+      for (const elem of carousel.children) {
+        if (elem.tagName.toLocaleLowerCase() === "div") {
+          maxCount++;
+          elem.style.transform = `translateX(${n}px)`;
+          elem.style.transition = "none";
+        }
       }
     }
   };
@@ -20,9 +37,9 @@ window.addEventListener("load", () => {
     return width + n > width ? "prev" : "next";
   };
 
-  // EVENTS
   const nextItem = () => {};
   const prevItem = () => {};
+
   const touchStart = (evt) => {
     control = true;
     firstTouchX = evt.x - carousel.offsetLeft;
@@ -31,22 +48,41 @@ window.addEventListener("load", () => {
 
   const touchMove = (evt) => {
     if (control) {
-      move((firstTouchX - (evt.x - carousel.offsetLeft)) * -1 - index * width);
-      let result = moveDirection((firstTouchX - (evt.x - carousel.offsetLeft)) * -1);
+      move((firstTouchX - (evt.x - carousel.offsetLeft)) * -1 - index * width, { delay: false });
+      moveDirection((firstTouchX - (evt.x - carousel.offsetLeft)) * -1);
     }
   };
+
   const touchStop = () => {
-    console.log("touchStop");
     control = false;
-    index++;
-    move(index * width * -1);
+    let minMove = Math.abs(width * index + lastMoveX) > 100;
+
+    if (direction === "next" && minMove && index + 1 < maxCount) {
+      index++;
+    }
+
+    if (direction === "prev" && minMove && index > 0) {
+      index--;
+    }
+
+    move(index * width * -1, { delay: true });
   };
+
   const touchCancel = () => {
-    control = false;
+    touchStop();
   };
+
+  // MOUSE EVENTS
 
   carousel.addEventListener("mousedown", (evt) => touchStart(evt));
   carousel.addEventListener("mousemove", (evt) => touchMove(evt));
   carousel.addEventListener("mouseleave", (evt) => touchCancel(evt));
   carousel.addEventListener("mouseup", (evt) => touchStop(evt));
+
+  // TOUCH EVENTS
+
+  carousel.addEventListener("touchstart", (evt) => touchStart(evt));
+  carousel.addEventListener("touchmove", (evt) => touchMove(evt));
+  carousel.addEventListener("touchcancel", (evt) => touchCancel(evt));
+  carousel.addEventListener("touchend", (evt) => touchStop(evt));
 });
